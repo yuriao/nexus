ef6966612868be26be05557e12a23908145eb790
lujia chen <lujiac1@uci.edu>
2026-04-03 23:29:16 -0400

    fix(ws): remove AllowedHostsOriginValidator to allow cross-origin WS from GitHub Pages

--- services/core-api/core_api/asgi.py
+++ services/core-api/core_api/asgi.py
@@ -6,7 +6,6 @@ import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from django.urls import path
 
@@ -25,8 +24,6 @@ websocket_urlpatterns = [
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
-        ),
+        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
     }
 )
